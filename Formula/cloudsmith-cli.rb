504e53ddb9e3477f96758005b58ba61e8fd0c758
Ranjan Singh <rsingh@cloudsmith.io>
2026-02-13 16:11:28 +0530

    chore: remove backup file

--- Formula/cloudsmith-cli.rb
+++ Formula/cloudsmith-cli.rb
@@ -6,21 +6,21 @@ class CloudsmithCli < Formula
   sha256 "78e70e148fabd17be225b345ad4dce0b7f686ecb1b24a1a34a3460d89a232f95"
   license "Apache-2.0"
 
-  # The PEX/zipapp bundles all Python dependencies, so we only need Python 3
+  # The PEX/zipapp bundles all Python dependencies, so we only need Python 3.10
   depends_on "python@3.10"
 
   def install
     # Install the zipapp directly to libexec
     libexec.install "cloudsmith.pyz"
-    
+
     # Make the zipapp executable
     chmod 0755, libexec/"cloudsmith.pyz"
-    
+
     # Create a wrapper script in bin that executes the zipapp
     (bin/"cloudsmith").write_env_script libexec/"cloudsmith.pyz", {}
   end
 
   test do
-    assert_match "1.12.1", shell_output("#{bin}/cloudsmith --version")
+    assert_match version.to_s, shell_output("#{bin}/cloudsmith --version")
   end
 end
