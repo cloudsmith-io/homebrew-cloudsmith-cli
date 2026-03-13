0853ccf96f61af22fd7b2ff1fc7d85147b521dbc
colinmoynes <cmoynes@cloudsmith.io>
2026-03-13 13:49:03 +0000

    bump release to v.1.14.0

--- Formula/cloudsmith-cli.rb
+++ Formula/cloudsmith-cli.rb
@@ -2,8 +2,8 @@
 class CloudsmithCli < Formula
   desc "Official Cloudsmith Command-Line Interface - Be Awesome. Automate Everything"
   homepage "https://help.cloudsmith.io/docs/cli/"
-  url "https://github.com/cloudsmith-io/cloudsmith-cli/releases/download/v1.13.0/cloudsmith.pyz"
-  sha256 "4daeb37e0fecacc3b90150b4d61be97065d9dbfdbd015eb8df9baf5c212df997"
+  url "https://github.com/cloudsmith-io/cloudsmith-cli/releases/download/v1.14.0/cloudsmith.pyz"
+  sha256 "affff5167a1007295a95c84b7e1efa94ad39d7ab820ee52f36ccc1a66cfb523a"
   license "Apache-2.0"
 
   # The PEX/zipapp bundles all Python dependencies, so we only need Python 3.10
