f7ae89955c0630e7f69f4974287f46be7f783531
Colin Moynes <77994705+colinmoynes@users.noreply.github.com>
2026-03-20 14:40:05 +0000

    fix sha version (#19)

--- Formula/cloudsmith-cli.rb
+++ Formula/cloudsmith-cli.rb
@@ -3,7 +3,7 @@ class CloudsmithCli < Formula
   desc "Official Cloudsmith Command-Line Interface - Be Awesome. Automate Everything"
   homepage "https://help.cloudsmith.io/docs/cli/"
   url "https://github.com/cloudsmith-io/cloudsmith-cli/releases/download/v1.15.0/cloudsmith.pyz"
-  sha256 "ae05a95289f26758166d66ea72d9de109febd4164017168c4bcff58fd0ff461c"
+  sha256 "7b8b454deaa3d8c9149aab80adb25aa7fa2690f058bf37aad54b2edff100dfaa"
   license "Apache-2.0"
 
   # The PEX/zipapp bundles all Python dependencies, so we only need Python 3.10
